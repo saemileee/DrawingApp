8da68e4c8ad48feb14e784960995549ad119e5fe
saemileee <68241138+saemileee@users.noreply.github.com>
2023-01-29 18:26:47 +0900

    23-01-29 18:26 Color Option

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas"); 
@@ -29,9 +30,17 @@ function onLineWidthChange(event){
     ctx.lineWidth = event.target.value; //range의 변경된 value 값을 잡아주는 func
 }
 
+
 function onColorChange(event){
     ctx.strokeStyle = event.target.value;
     ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event){
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -40,3 +49,4 @@ canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting); // 마우스가 캔버스를 떠났을 때 계속 끌고가는 버그를 잡아주는 것
 lineWidth.addEventListener("change", onLineWidthChange)
 color.addEventListener("change", onColorChange);
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
