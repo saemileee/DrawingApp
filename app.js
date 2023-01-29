1c126a19fa2e7db9682fb9f46e446ac6077dfbe2
saemileee <68241138+saemileee@users.noreply.github.com>
2023-01-29 18:02:19 +0900

    23-01-29 18:01 Line Color Changer

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas"); 
 const ctx = canvas.getContext("2d"); //context는 캔버스에 그림을 그리는 붓이라고 생각하면 됨
@@ -28,9 +29,14 @@ function onLineWidthChange(event){
     ctx.lineWidth = event.target.value; //range의 변경된 value 값을 잡아주는 func
 }
 
+function onColorChange(event){
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting); // 마우스가 캔버스를 떠났을 때 계속 끌고가는 버그를 잡아주는 것
-
 lineWidth.addEventListener("change", onLineWidthChange)
+color.addEventListener("change", onColorChange);
