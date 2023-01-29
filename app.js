96d625106b28dbc2bcfcbe3117a9a58dd517cf50
saemileee <68241138+saemileee@users.noreply.github.com>
2023-01-29 17:53:24 +0900

    23-01-29 17:53 line width changer

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas"); 
 const ctx = canvas.getContext("2d"); //context는 캔버스에 그림을 그리는 붓이라고 생각하면 됨
 canvas.width = 800; 
 canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
-ctx.fill();
+function onMove(event){
+    if(isPainting == true){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting(){
+    isPainting = true;
+}
+
+function cancelPainting() {
+    isPainting = false;
+    ctx.beginPath(); //mouseup할 때마다 새로운 패쓰로 시작하게 해주는 것
+}
+
+function onLineWidthChange(event){
+    ctx.lineWidth = event.target.value; //range의 변경된 value 값을 잡아주는 func
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting); // 마우스가 캔버스를 떠났을 때 계속 끌고가는 버그를 잡아주는 것
+
+lineWidth.addEventListener("change", onLineWidthChange)
